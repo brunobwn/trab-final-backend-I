d9fe9aee67c9dab1241e77e95d3eb544698fd796
brunobwn <brunoberwian@gmail.com>
2023-04-13 22:32:22 -0300

    Revert "remove parametros nao utilizados na função errorHandler"
    This reverts commit da6004a5f5852941e4017436bc15943a69380896.

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { z } from 'zod';
 
-function errorHandler(err:any) {
+function errorHandler(err:any, req: Request, res: Response) {
 	if (err instanceof z.ZodError) {
 		const issues = err.issues.map((issue) => ({ path: issue.path[0], message: issue.message }));
 		return res.status(400).json(issues);
